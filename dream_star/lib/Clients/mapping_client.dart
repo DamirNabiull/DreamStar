b8b81467f93e4a7b08b41c7730a54421fad0f5b0
Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>
2023-04-01 18:06:02 +0300

    Added mapping_client

---
+++ dream_star/lib/Clients/mapping_client.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:dream_star/DTO/task_dto.dart';
+import 'package:dream_star/Models/task_info.dart';
+import 'package:riverpod/riverpod.dart';
+
+class MappingClient {
+  TaskInfo taskDTOToTaskInfo(TaskDTO taskDTO) =>
+      TaskInfo(
+          taskDTO.title,
+          taskDTO.description,
+          taskDTO.cost,
+          _taskStatusToEnum(taskDTO.status),
+          _stringToColor(taskDTO.color),
+          taskDTO.childName,
+          taskDTO.childId
+      );
+
+  TaskDTO taskInfoToTaskDTO(TaskInfo task) =>
+      TaskDTO(
+          childId: task.childId,
+          childName: task.childName,
+          title: task.title,
+          description: task.description,
+          status: _taskStatusToString(task.status),
+          cost: task.cost,
+          color: _colorToString(task.lineColor)
+      );
+
+  TaskStatus _taskStatusToEnum(String status) =>
+      TaskStatus.values.firstWhere(
+              (e) => e.toString() == 'TaskStatus.$status'
+      );
+
+  String _taskStatusToString(TaskStatus status) => status.toString();
+
+  Color _stringToColor(String color) {
+    return const Color(0x00000000);
+  }
+
+  String _colorToString(Color color) => color.toString();
+}
+
+final mappingProvider = Provider((ref) => MappingClient());
