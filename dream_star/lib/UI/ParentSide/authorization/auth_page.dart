d12b6b6aba3aeb0d5078967cf4878ae0c33caf8c
Arseniy172 <LEvochkin-2002@mail.ru>
2023-04-04 12:25:23 +0300

    auth parent page

---
+++ dream_star/lib/UI/ParentSide/authorization/auth_page.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+// import 'package:localization/localization.dart';
+
+class AuthParentScreen extends StatefulWidget {
+  const AuthParentScreen({super.key});
+
+  @override
+  AuthParentScreenState createState() => AuthParentScreenState();
+}
+
+enum SingingCharacter { lafayette, jefferson }
+
+class AuthParentScreenState extends State<AuthParentScreen> {
+  SingingCharacter? _character = SingingCharacter.lafayette;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const SizedBox(height: 30),
+              Align(
+                alignment: Alignment.topLeft,
+                child: ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.grey,
+                    size: 20.0,
+                  ),
+                  label: const Text('Изменить роль',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 18,
+                          color: Colors.grey)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                  ),
+                  onPressed: () {
+                    print('Button Pressed');
+                  },
+                ),
+              ),
+              const SizedBox(height: 30),
+              Align(
+                alignment: Alignment.topLeft,
+                child: RichText(
+                  text: const TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Создайте аккаунт!',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.deepPurpleAccent)),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Align(
+                alignment: Alignment.topLeft,
+                child: RichText(
+                  text: const TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Для начала, зарегистрируемся',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13,
+                              color: Colors.grey)),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey)),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Имя',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey)),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Фамилия',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey)),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Дата рождения',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  // color: Colors.white,
+                  width: 40.0,
+                  height: 21.0,
+                  child: Column(children: const [
+                    Text("Пол"),
+                  ]),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey)),
+                ),
+              ),
+              // const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(1.0),
+                    width: 146,
+                    child: RadioListTile<SingingCharacter>(
+                      controlAffinity: ListTileControlAffinity.trailing,
+                      title: const Text('Женский',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal, fontSize: 15)),
+                      value: SingingCharacter.lafayette,
+                      groupValue: _character,
+                      onChanged: (SingingCharacter? value) {
+                        setState(() {
+                          _character = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(1.0),
+                    width: 146,
+                    child: RadioListTile<SingingCharacter>(
+                      controlAffinity: ListTileControlAffinity.trailing,
+                      title: const Text('Мужской',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal, fontSize: 15)),
+                      value: SingingCharacter.jefferson,
+                      groupValue: _character,
+                      onChanged: (SingingCharacter? value) {
+                        setState(() {
+                          _character = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey)),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Электронная почта',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey)),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Пароль',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey)),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Подтвердите пароль',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Button Pressed');
+                  },
+                  child: Text('Зарегистрироваться'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurpleAccent,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 20),
+                    textStyle: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(children: <Widget>[
+                  RichText(
+                    text: const TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'Вы тут не впервые?',
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 13,
+                                color: Colors.grey)),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 13),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Войдите!',
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 13,
+                            color: Colors.deepPurpleAccent)),
+                  ),
+                ]),
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
