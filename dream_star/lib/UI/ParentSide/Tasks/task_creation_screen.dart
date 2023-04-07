9fa5d771efeea34a9390d75ef0b7dbfd6c9e322c
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2023-04-07 22:06:30 +0300

    create task screen

--- dream_star/lib/UI/ParentSide/Tasks/task_creation_screen.dart
+++ dream_star/lib/UI/ParentSide/Tasks/task_creation_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dream_star/UI/Components/custom_date_input_field.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:localization/localization.dart';
 import '../../Components/custom_text_field.dart';
 import '../../Components/unified_fields.dart';
@@ -14,14 +16,60 @@ class TaskCreationScreen extends StatefulWidget {
 }
 
 class TaskCreationScreenState extends State<TaskCreationScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
   int dropdownCostValue = taskCostList.first;
   late String dropdownNameValue;
-
+  bool deadlineFlag = false;
+  final TextEditingController _dateInput = TextEditingController();
+  bool penaltyFlag = false;
+  late List<int> taskPenaltyList;
+  late int dropdownPenaltyValue;
+  bool isButtonDisabled = true;
 
   @override
   void initState() {
     super.initState();
     dropdownNameValue = widget.childrenNamesList.first;
+    _dateInput.text = DateFormat('dd.MM.yyyy').format(DateTime.now());
+    taskPenaltyList = List<int>.generate(dropdownCostValue, (i) => i + 1);
+    dropdownPenaltyValue = taskPenaltyList.first;
+
+    _titleController.addListener(() {
+      final String text = _titleController.text;
+
+      setState(() {
+        if (text.isEmpty){
+          isButtonDisabled = true;
+        } else {
+          isButtonDisabled = false;
+        }
+      });
+
+      _titleController.value = _titleController.value.copyWith(
+        text: text,
+        selection:
+            TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+
+    _descriptionController.addListener(() {
+      final String text = _descriptionController.text;
+      _descriptionController.value = _descriptionController.value.copyWith(
+        text: text,
+        selection:
+            TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
   }
 
   @override
@@ -34,28 +82,45 @@ class TaskCreationScreenState extends State<TaskCreationScreen> {
                 color: Colors.white,
                 fontWeight: FontWeight.w500,
                 fontSize: 17.0)),
-        backgroundColor: const Color(0x00F6F5FF),
         body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 28, horizontal: 16),
-          child: Column(
-            children: [
-              buildDescriptionSection(),
-              const SizedBox(height: 12),
-              buildChildSection(),
-              const SizedBox(height: 12),
-              buildCostSection(),
-              const SizedBox(height: 12),
-            ],
-          ),
-        ));
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+            child: Column(
+              children: [
+                Expanded(
+                    child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 28),
+                      buildDescriptionSection(),
+                      const SizedBox(height: 12),
+                      buildChildSection(),
+                      const SizedBox(height: 12),
+                      buildCostSection(),
+                      const SizedBox(height: 12),
+                      buildDeadlineSection(),
+                      const SizedBox(height: 12),
+                      buildPenaltySection()
+                    ],
+                  ),
+                )),
+                buildButton(),
+                const SizedBox(height: 20),
+              ],
+            )));
   }
 
   Widget buildDescriptionSection() {
     return UnifiedFields(
         widget1: CustomTextField(
-            TextEditingController(), 'task-title'.i18n(), 1, 25),
-        widget2: CustomTextField(TextEditingController(),
-            'task-description'.i18n(), 1, 125));
+            textEditingController: _titleController,
+            placeholder: 'task-title'.i18n(),
+            maxLines: 1,
+            maxLength: 35),
+        widget2: CustomTextField(
+            textEditingController: _descriptionController,
+            placeholder: 'task-description'.i18n(),
+            maxLines: 5,
+            maxLength: 125));
   }
 
   Widget buildChildSection() {
@@ -72,17 +137,19 @@ class TaskCreationScreenState extends State<TaskCreationScreen> {
               value: dropdownNameValue,
               isDense: true,
               borderRadius: BorderRadius.circular(12.0),
-              icon: const Icon(Icons.unfold_more_rounded,
-                  color: Colors.black),
+              icon: const Icon(Icons.unfold_more_rounded, color: Colors.black),
               style: const TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.w400, fontSize: 17.0),
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 17.0),
               underline: const SizedBox.shrink(),
               onChanged: (String? value) {
                 setState(() {
                   dropdownNameValue = value!;
                 });
               },
-              items: widget.childrenNamesList.map<DropdownMenuItem<String>>((String value) {
+              items: widget.childrenNamesList
+                  .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -91,8 +158,7 @@ class TaskCreationScreenState extends State<TaskCreationScreen> {
             )
           ],
         ),
-        widget2: null
-    );
+        widget2: null);
   }
 
   Widget buildCostSection() {
@@ -108,15 +174,22 @@ class TaskCreationScreenState extends State<TaskCreationScreen> {
             DropdownButton<int>(
               value: dropdownCostValue,
               isDense: true,
+              alignment: AlignmentDirectional.centerEnd,
               borderRadius: BorderRadius.circular(12.0),
-              icon: const Icon(Icons.unfold_more_rounded,
-                  color: Colors.black),
+              icon: const Icon(Icons.unfold_more_rounded, color: Colors.black),
               style: const TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.w400, fontSize: 17.0),
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 17.0),
               underline: const SizedBox.shrink(),
               onChanged: (int? value) {
                 setState(() {
                   dropdownCostValue = value!;
+                  if (dropdownCostValue < dropdownPenaltyValue) {
+                    dropdownPenaltyValue = dropdownCostValue;
+                  }
+                  taskPenaltyList =
+                      List<int>.generate(dropdownCostValue, (i) => i + 1);
                 });
               },
               items: taskCostList.map<DropdownMenuItem<int>>((int value) {
@@ -129,5 +202,109 @@ class TaskCreationScreenState extends State<TaskCreationScreen> {
           ],
         ),
         widget2: null);
+  }
+
+  Widget buildDeadlineSection() {
+    return UnifiedFields(
+        widget1: Row(children: [
+          Text('deadline-title'.i18n(),
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 17.0)),
+          const Spacer(),
+          Container(
+            constraints: const BoxConstraints(maxHeight: 25.0),
+            child: Switch(
+                value: deadlineFlag,
+                activeColor: Colors.deepPurpleAccent,
+                onChanged: (bool value) {
+                  setState(() {
+                    deadlineFlag = value;
+                  });
+                }),
+          )
+        ]),
+        widget2:
+            deadlineFlag ? CustomDateInputField(dateInput: _dateInput) : null);
+  }
+
+  Widget buildPenaltySection() {
+    return UnifiedFields(
+        widget1: Row(children: [
+          Text('penalty-title'.i18n(),
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 17.0)),
+          const Spacer(),
+          Container(
+            constraints: const BoxConstraints(maxHeight: 25.0),
+            child: Switch(
+                value: penaltyFlag,
+                activeColor: Colors.deepPurpleAccent,
+                onChanged: (bool value) {
+                  setState(() {
+                    penaltyFlag = value;
+                  });
+                }),
+          )
+        ]),
+        widget2: penaltyFlag ? buildPenaltyCostSection() : null);
+  }
+
+  Widget buildPenaltyCostSection() {
+    return Row(
+      children: [
+        Text('penalty-cost-title'.i18n(),
+            style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+                fontSize: 17.0)),
+        const Spacer(),
+        DropdownButton<int>(
+          value: dropdownPenaltyValue,
+          isDense: true,
+          borderRadius: BorderRadius.circular(12.0),
+          alignment: AlignmentDirectional.centerEnd,
+          icon: const Icon(Icons.unfold_more_rounded, color: Colors.black),
+          style: const TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w400, fontSize: 17.0),
+          underline: const SizedBox.shrink(),
+          onChanged: (int? value) {
+            setState(() {
+              dropdownPenaltyValue = value!;
+            });
+          },
+          items: taskPenaltyList.map<DropdownMenuItem<int>>((int value) {
+            return DropdownMenuItem<int>(
+              value: value,
+              child: Text(value.toString()),
+            );
+          }).toList(),
+        )
+      ],
+    );
+  }
+
+  Widget buildButton() {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 5,
+          disabledBackgroundColor: Colors.lightGreenAccent,
+          backgroundColor: Colors.deepPurpleAccent,
+          shadowColor: Colors.grey,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(22.0)
+          ),
+          minimumSize: const Size.fromHeight(54),
+          textStyle: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w700, fontSize: 17.0),
+          ),
+        onPressed: isButtonDisabled ? null : (){},
+        child: Text(
+          'create-task'.i18n(),
+        )
+    );
   }
 }
