3af579e8f69696651e5b5ea813aa008d01f163a9
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2023-04-09 17:14:20 +0300

    Update connection_client.dart
    Co-Authored-By: Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>

--- dream_star/lib/Clients/connection_client.dart
+++ dream_star/lib/Clients/connection_client.dart
@@ -3,36 +3,47 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ConnectionClient extends Notifier<bool> {
   ConnectionClient() {
-    final internetProvider = InternetConnectionChecker.createInstance(
-      checkTimeout: const Duration(seconds: 2),
-      checkInterval: const Duration(milliseconds: 400),
-    );
+    if (!kIsWeb) {
+      final internetProvider = InternetConnectionChecker.createInstance(
+        checkTimeout: const Duration(seconds: 2),
+        checkInterval: const Duration(milliseconds: 400),
+      );
 
-    final StreamSubscription<InternetConnectionStatus> _ =
-        internetProvider.onStatusChange.listen(
-      (InternetConnectionStatus status) {
-        // print("CHECK");
-        switch (status) {
-          case InternetConnectionStatus.connected:
-            state = true;
-            break;
-          case InternetConnectionStatus.disconnected:
-            state = false;
-            break;
+      final StreamSubscription<InternetConnectionStatus> _ =
+          internetProvider.onStatusChange.listen(
+        (InternetConnectionStatus status) {
+          // print("CHECK");
+          switch (status) {
+            case InternetConnectionStatus.connected:
+              state = true;
+              break;
+            case InternetConnectionStatus.disconnected:
+              state = false;
+              break;
+          }
+        },
+      );
+
+      Connectivity().onConnectivityChanged.listen((result) async {
+        state = await InternetConnectionChecker().hasConnection;
+      });
+    } else {
+      Connectivity().onConnectivityChanged.listen((result) async {
+        if (result == ConnectivityResult.none) {
+          state = false;
+        } else {
+          state = true;
         }
-      },
-    );
-
-    Connectivity().onConnectivityChanged.listen((result) async {
-      state = await InternetConnectionChecker().hasConnection;
-    });
+      });
+    }
   }
 
   @override
   bool build() {
-    return false;
+    return true;
   }
 }
