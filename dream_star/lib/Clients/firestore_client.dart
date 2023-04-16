74b3523498da9f67ab9f9809b0311cf2d9eeccd3
Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>
2023-04-16 20:19:52 +0300

    Added userInfo READ/WRITE

--- dream_star/lib/Clients/firestore_client.dart
+++ dream_star/lib/Clients/firestore_client.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dream_star/Clients/providers.dart';
 import 'package:dream_star/DTO/task_dto.dart';
+import 'package:dream_star/DTO/user_dto.dart';
 import 'package:dream_star/Models/task_info.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,6 +11,7 @@ import 'mapping_client.dart';
 class FireStoreClient {
   final FirebaseFirestore db = FirebaseFirestore.instance;
   late final CollectionReference<Map<String, dynamic>> _tasksCollection;
+  late final CollectionReference<Map<String, dynamic>> _usersCollection;
   // late final CollectionReference<Map<String, dynamic>> _dreamsCollection;
 
   final container = ProviderContainer();
@@ -17,6 +19,7 @@ class FireStoreClient {
 
   FireStoreClient() {
     _tasksCollection = db.collection("tasks");
+    _usersCollection = db.collection("users");
     // _dreamsCollection = db.collection("dreams");
   }
 
@@ -26,45 +29,6 @@ class FireStoreClient {
     taskDTO.id = taskRef.id;
 
     await taskRef.set(taskDTO.toJson());
-  }
-
-  void createTestTasks() async {
-    var titles = [
-      "Постирать вещи",
-      "Сделать ДЗ",
-      "Пропылесосить дом",
-      "Выгулять собаку",
-      "Помыть посуду",
-      "Сходить в магазин",
-      "Расписание в дневнике"
-    ];
-    var descriptions = [
-      "Собери свои белые вещи и белые вещи из контейнера с грязными вещами, режим на машинке поставь \"Хлопок\", порошок для белых вещей лежит в шкафу.",
-      "Сделай домашку до моего прихода, когда приду и проверю - получишь свою звёздочку.",
-      "Пропылесось, пожалуйста, все комнаты, и не забудь про ванную и туалет.",
-      "Выгуляй Персика, пожалуйста, и не забудь помыть ему лапки, как домой зайдёте.",
-      "Помой посуду, после того, как поешь. Курица с рисом в холодильнике.",
-      "Купи молоко, батон, сыр, конфеты какие-нибудь, печенье и макароны.",
-      "Заполни дневник на две недели вперёд."
-    ];
-
-    var statuses = [TaskStatus.progress, TaskStatus.review, TaskStatus.passed];
-
-    for (var i = 0; i < 42; i++) {
-      var taskRef = _tasksCollection.doc();
-
-      TaskDTO task = TaskDTO(
-          "test",
-          "test",
-          "${titles[i % 7]} test_$i",
-          descriptions[i % 7],
-          statuses[(i ~/ 7) % 3].toString(),
-          i % 11 + 1,
-          i % 17,
-          taskRef.id);
-
-      taskRef.set(task.toJson());
-    }
   }
 
   void updateTaskStatus(String taskId, TaskStatus status) async {
@@ -84,9 +48,15 @@ class FireStoreClient {
             .toList());
   }
 
-  Stream<List<TaskInfo>> getAllTasks() {
-    return _tasksCollection.snapshots().map((snapshot) => snapshot.docs
-        .map((doc) => _mapper.taskDTOToTaskInfo(TaskDTO.fromJson(doc.data())))
-        .toList());
+  void createUser(UserDTO userDTO, String id) {
+    _usersCollection.doc(id).set(userDTO.toJson());
+  }
+
+  Future<UserDTO?> getUser(String id) async {
+    final snapshot = await _usersCollection.doc(id).get();
+    if (snapshot.exists) {
+      return UserDTO.fromJson(snapshot.data()!);
+    }
+    return null;
   }
 }
